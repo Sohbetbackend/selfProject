30bd98d87c7330d919475085bd4225381c4f1d2e
Sohbetbackend <sgurbanov888@gmail.com>
2024-01-05 19:21:53 +0500

    fix+add: file_name+gitignore

--- database/migrations/library_up.sql
+++ database/migrations/library_up.sql
@@ -3,16 +3,16 @@ create table books (
     name varchar(255) NOT NULL,
     page bigint DEFAULT NULL,
     category_id bigint DEFAULT NULL REFERENCES categories ON DELETE CASCADE,
-    author_id bigint DEFAULT NULL REFERENCES authors ON DELETE CASCADE,
-)
+    author_id bigint DEFAULT NULL REFERENCES authors ON DELETE CASCADE
+);
 
 create table categories (
     id serial primary KEY,
-    name varchar(255) NOT NULL, 
-)
+    name varchar(255) NOT NULL
+);
 
 create table authors (
     id serial primary KEY,
     first_name varchar(255) NOT NULL,
-    last_name varchar(255) DEFAULT NULL,
-)
+    last_name varchar(255) DEFAULT NULL
+);
